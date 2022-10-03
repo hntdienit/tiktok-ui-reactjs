1f4385f89cf15c40539963766f3451d8a88facd9
hntdienit <hntdienit@gmail.com>
2022-10-03 11:54:53 +0700

    thu

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ const {
 } = require("customize-cra");
 
 module.exports = override(
-    // enable legacy decorators babel plugin
+    // enable legacy decorators babel plugin //thanhdien
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useBabelRc()
 );
